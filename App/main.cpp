50de373ec5bba1e6b5d8afa252aa8ebef6a1f79f
tpapavas <tpapavas@csd.auth.gr>
2022-04-08 12:11:27 +0300

    added some comments in main. removed "using namespace std" in ocr_tabs.cpp

--- App/main.cpp
+++ App/main.cpp
@@ -17,12 +17,12 @@ int main(int argc, char *argv[])
 		std::string filename(argv[1]);
 		if (filename.empty()) return -1;
 		char buffer[MAX_PATH];
-		GetModuleFileNameA( NULL, buffer, MAX_PATH );
-		std::string::size_type pos = std::string( buffer ).find_last_of( "\\/" );
+		GetModuleFileNameA(NULL, buffer, MAX_PATH);		//get .exe path "<path-to-project>\\<exe-folder>\\<program>.exe"
+		std::string::size_type pos = std::string( buffer ).find_last_of( "\\/" );	//remove "<program>.exe" from path	
 		std::string currentPath = std::string( buffer ).substr( 0, pos);
 		pos = currentPath.find_last_of( "\\/" );
-		currentPath = std::string( buffer ).substr( 0, pos);
-		filename = currentPath + "\\" + filename;
+		currentPath = std::string( buffer ).substr( 0, pos);	//remove "<exe-folder>\\" from path; point to root of project "<path-to-project>".
+		filename = currentPath + "\\" + filename;	//get path to input file "<path-to-project>\\<relative-path-to-file>"
 		ocr_tabs tab;
 		if (((filename.find(".pdf")!=std::string::npos) && (tab.pdf2html(filename))) ||  tab.img2html(filename) ) return 1;
 		else return -1;
